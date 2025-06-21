092854cb1d08d3b921b381587ca2cf175b23de32
Your Name <your.email@example.com>
2025-06-21 15:30:46 +0530

    backend

---
+++ backend/Qkey.py
@@ -0,0 +1,20 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit_aer import AerSimulator
+
+
+
+def main():
+
+    textsizelimit= 1024
+    message= "HEY BLYAT"
+
+    
+
+
+
+
+
+
+
+if __name__== '__main__':
+    main()
