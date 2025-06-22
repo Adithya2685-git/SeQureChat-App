6483e5839d8c440baf78d88ec6e2d4506c12bc21
Your Name <your.email@example.com>
2025-06-22 23:34:49 +0530

    sockets

--- backend/Qkey.py
+++ backend/Qkey.py
@@ -1,25 +1,32 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit_aer import AerSimulator
+from qiskit import transpile
+
 
 #basis gates qubit pairs for ALice and Bob Each
 # if 4096 then 4096 for alice and bob
 
     #Aer simulate it for number of qubits required size
     # Shots = qubits /4
-def measure(qc,shots):
+def measure(circuits,shots):
     simulator= AerSimulator()
-    job= simulator.run(qc, shots=shots,memory= True)
+
+    # Ensure `circuits` is a list
+    if not isinstance(circuits, list):
+        circuits = [circuits]
+
+    job= simulator.run(circuits, shots=shots,memory= True)
     result = job.result()
-    bitstrings = result.get_memory()
 
     finalstring=""
-    for bitstring in bitstrings:
+    
+    for i in range(len(circuits)):
+        bitstrings = result.get_memory(i)
+        for bitstring in bitstrings:
             finalstring+= bitstring
-
     
     finalstring= finalstring.replace(" ", "")
     return finalstring
-
 
 
 def rng(qubits):
@@ -50,7 +57,7 @@ def main():
     for i in range(totalcircuitscount):
         qc= QuantumCircuit(paircounts*2,paircounts*2)
         for j in range(paircounts):
-             basisindex=i*4+j
+             basisindex=i*paircounts+j
 
              qc.h(j*2)
              qc.cx(j*2,j*2+1)
@@ -59,21 +66,34 @@ def main():
                  qc.h(j*2)
              if Bobbasis[basisindex]=='1':
                  qc.h(j*2+1)
-                 
+
         qc.measure_all()
         circuits.append(qc)
-             
+
+    transpiled_circuits = transpile(circuits, AerSimulator())
+    measuredstring= measure(transpiled_circuits,1)
+
+    alicekey=""
+    bobkey  =""
+
+    for i in range(len(measuredstring)):
+        if i%2==0:
+            alicekey+= measuredstring[i]
+        else :
+            bobkey+= measuredstring[i]
 
 
-                 
 
+    #After measurement,  remove bits where basis isn't the same from keys
+    finalbasis=""
+    for i in range(len(Alicebasis)):
+        if Alicebasis[i]!= Bobbasis[i]:
+            alicekey= alicekey[:i]+ alicekey[i+1:]
+            bobkey= bobkey[:i]+ bobkey[i+1:]
+        else:
+            finalbasis+= Alicebasis
 
-    #print(qc.draw())
-
-
-
-
-
+    
 
 
 if __name__== '__main__':
