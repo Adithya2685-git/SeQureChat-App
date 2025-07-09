eca77478106db0452447738839b2a4164f88ac53
Your Name <your.email@example.com>
2025-07-09 23:30:43 +0530

    chatbg

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -1,9 +1,16 @@
 import './Chat.css';
-
+import chatbackground from '../../assets/chatbackground.jpg'
 function Chat(){
 
     return(<>
+        <img src={chatbackground} className='chat-background'></img>
+        <div className='chats-list'>
+        <h1>This area will show the users chats list</h1>
+        </div>
 
+        <div className='current-chat'>
+            <h1>This area will show the current chat with the selected user</h1>
+        </div>
     </>)
 }
 
